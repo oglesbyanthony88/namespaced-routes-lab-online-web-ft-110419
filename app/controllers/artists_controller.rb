5c2663a49b71fea979eef663fb8e4288bae81be8
Anthony Oglesby <ogle1ant@gmail.com>
2020-02-08 13:32:14 -0500

    Done.

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -1,4 +1,6 @@
 class ArtistsController < ApplicationController
+  
+
   def index
     @artists = Artist.all
   end
@@ -8,7 +10,12 @@ class ArtistsController < ApplicationController
   end
 
   def new
-    @artist = Artist.new
+     @preference = Preference.first
+     if @preference.allow_create_artists
+       @artist = Artist.new
+     else
+       redirect_to artists_path
+     end
   end
 
   def create
